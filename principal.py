a7682527ba46f0994008638944b1f9039bab0970
vieirapaulobnu <vieirapaulobnu@gmail.com>
2026-06-18 21:49:34 -0300

    env

---
+++ principal.py
@@ -0,0 +1,33 @@
+from modelos import Produto, listar_produtos
+
+def exibir_menu():
+    print("=======================")
+    print("MENU DE PRODUTOS")
+    print("0 - Sair")
+    print("1 - Cadastrar")
+    print("2 - Listar")
+    print("=======================")
+
+def cadastrar():
+    nome = input("Digite o nome: ")
+    preco = float(input("Digite o preço:"))
+    categoria = input("Digite a categoria: ") 
+
+    produto = Produto(nome, preco, categoria)
+    produto.salvar()
+
+def mostrar():
+    for produto in listar_produtos():
+        produto.exibir()
+
+    while True:
+        opcao = input("Digite uma opção: ")
+
+        if opcao == "0":
+            break
+        elif opcao == "1":
+            cadastrar()
+        elif opcao == "2":
+             mostrar()
+        else:
+            print("Opção inválida! Tente novamente.")
